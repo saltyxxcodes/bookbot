373116697d717ef0a95ead05d4e5cf34cb873c24
saltyxxcodes <saltysuzi.official@gmail.com>
2024-06-18 21:56:49 +0200

    added character counting function to main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_counter = count_words(text)
-    print(f"{word_counter} words found in the document")
+    character_counter = count_characters(text)
+    print(character_counter)
 
 
 def get_book_text(path):
@@ -13,5 +14,17 @@ def count_words(besedilo):
     words = besedilo.split()
     return len(words)
 
+def count_characters(besedilo):
+    alphabet = {}
+    for crka in besedilo:
+        mala_crka = crka.lower()
+        if mala_crka in alphabet:
+            alphabet[mala_crka] += 1
+        else:
+            alphabet[mala_crka] = 1
+
+    return alphabet
+    
+    
 main()
 
