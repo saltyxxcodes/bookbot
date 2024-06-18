cf443fb6c73a74607b2373bf1c14c24734ce3f78
saltyxxcodes <saltysuzi.official@gmail.com>
2024-06-18 21:06:07 +0200

    update main.py with a new function

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    words = text.split()
+    stevilo_besed = 0
+    for word in words:
+        stevilo_besed += 1
+    print(stevilo_besed)
+
+
+main()
